9536b63477dcfacc2a7c8e5f7a19b38e4070db1d
Wagner Jafet Matos Pe�a <20220137@itla.edu.do>
2023-09-29 10:36:57 -0400

    Creacion de CarsView con Chat integrado

---
+++ WAPS08/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAPS08
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string usuario, string mensaje)
+        {
+            Clients.All.EnviarMensaje(usuario, mensaje);
+        }
+    }
+}
